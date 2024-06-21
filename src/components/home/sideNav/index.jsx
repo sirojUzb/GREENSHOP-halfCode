4afff37a8ace537ace5934b3f45cdca5b5aa4f96
sirojUz <sirojiddinmamatovuzb@gmail.com>
2024-06-21 22:16:35 +0900

    commit

--- src/components/home/sideNav/index.jsx
+++ src/components/home/sideNav/index.jsx
@@ -5,7 +5,7 @@ import Sizes from "./sizes";
 
 const SideNav = () => {
   return (
-    <div className="w-[310px] bg-[#f5f5f5] px-[18px] py-[14px]">
+    <div className="w-[311px] bg-[#f5f5f5] px-[18px] py-[14px]">
       <Categories />
       <PriceRange />
       <Sizes />
