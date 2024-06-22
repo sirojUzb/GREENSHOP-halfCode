c4fe282bbb5f8c629e2f345892024c6fcb62c21b
sirojUz <sirojiddinmamatovuzb@gmail.com>
2024-06-22 23:52:44 +0900

    sideNav sizes

--- src/components/home/sideNav/sizes/index.jsx
+++ src/components/home/sideNav/sizes/index.jsx
@@ -1,17 +1,37 @@
+import { useSearchParams } from "../../../../hooks/useSearchParams";
+
 const Sizes = () => {
+  const { getParams, setParams } = useSearchParams;
+
+  const normal_text =
+    "w-full flex justify-between items-center mt-[7px] hover:text-[#46a358] cursor-pointer";
+  const active_text =
+    "w-full flex justify-between items-center mt-[7px] text-[#46a358] cursor-pointer";
+
+  const selectedSize = getParams("size" ?? "small");
+
   return (
     <div className="mt-[38px]">
       <h3 className="font-bold">Size</h3>
       <div className="pl-[12px] w-full">
-        <div className="w-full flex justify-between items-center mt-[7px] hover:text-[#46A358] cursor-pointer">
+        <div
+          onClick={() => setParams({ size: "small" })}
+          className={selectedSize === "small" ? active_text : normal_text}
+        >
           <h3>Small</h3>
           <h3>(12)</h3>
         </div>
-        <div className="w-full flex justify-between items-center mt-[7px] hover:text-[#46A358] cursor-pointer">
+        <div
+          onClick={() => setParams({ size: "medium" })}
+          className={selectedSize === "medium" ? active_text : normal_text}
+        >
           <h3>Medium</h3>
           <h3>(12)</h3>
         </div>
-        <div className="w-full flex justify-between items-center mt-[7px] hover:text-[#46A358] cursor-pointer">
+        <div
+          onClick={() => setParams({ size: "large" })}
+          className={selectedSize === "large" ? active_text : normal_text}
+        >
           <h3>Large</h3>
           <h3>(12)</h3>
         </div>
