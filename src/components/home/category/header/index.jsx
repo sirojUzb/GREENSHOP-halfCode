e82b04e588a51bea0a223612b0c907d7138208b5
sirojUz <sirojiddinmamatovuzb@gmail.com>
2024-06-27 00:11:56 +0900

    category section

--- src/components/home/category/header/index.jsx
+++ src/components/home/category/header/index.jsx
@@ -6,6 +6,8 @@ const Header = () => {
   const activeSelection =
     "text-[#46a358] font-bold border-b-2 border-[#46a358]";
   const type = getParams("type");
+  const sort = getParams("sort");
+
 
   return (
     <div className="w-full flex justify-between items-center">
@@ -34,7 +36,34 @@ const Header = () => {
            Sale
         </h3>
       </div>
-      <div><Select labelInValue defaultInValue={{value:"",label:""}}/></div>
+      <div className="flex items-center gap-2">
+        <h3>Sort by</h3>
+        <Select 
+            labelInValue
+            defaultInValue={{
+                value: sort,
+            }}
+            onChange={(value) => setParams({ sort:value})}
+            style={{
+                width: 120,
+            }}
+            options={[
+                {
+                    value: "default-sorting",
+                    label: "Default Sorting",
+                },
+                {
+                    value: "the-cheapest",
+                    label: "The Cheapest",               
+                }
+                {
+                    value: "most-expensive",
+                    label: "Most expensive",
+                },
+            ]
+
+            }
+                /></div>
     </div>
   );
 };
